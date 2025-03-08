2e4e7b039e523972162e2355da4590984298ad55
FhantomWatcher <85093344+FarhanJuneidSiddiqui@users.noreply.github.com>
2025-03-08 23:05:19 +0600

    Upload java practice codes

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+	public static void main(String[] Fht){
+		System.out.println("Hello World");
+	}
+}
---
+++ Rulz/ClassRules.java
@@ -0,0 +1,15 @@
+public class ClassRules{
+	public static void main(String[] args){
+		System.out.println("Class rules of OOP[W]");
+		System.out.println("1.Attendence counted after 10 min late.");
+		System.out.println("2.No make up quiz without valid reason.");
+		System.out.println("3.No improvedment Quiz.");
+		System.out.println("4.Full Attendance without late  will get +2 bonus mark.");
+		System.out.println("5.No Phone during Lucture.");
+		System.out.println("6.Have to pass In mid by self");
+		System.out.println("7.Communication mediam is teams and mail");
+		System.out.println("8.Message alway on point.");
+		System.out.println("9.Consulting for specific topic.");
+		System.out.println("-------------- END ---------------");
+	}
+}
